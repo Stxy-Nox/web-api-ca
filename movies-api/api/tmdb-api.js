5ee565aa8edfb227f947b140ca78a7fe218ff7c7
SytxNox <seven_night_w@hotmail.com>
2025-01-02 16:05:01 +0000

    Add tmdb-api

--- movies-api/api/tmdb-api.js
+++ movies-api/api/tmdb-api.js
@@ -31,3 +31,48 @@ export const getGenres = async () => {
       throw error;
   }
 };
+
+export const getMovies = async () => {
+    try {
+        const response = await fetch(
+            `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=1`
+        );
+        if (!response.ok) {
+            throw new Error(response.json().message);
+        }
+
+        return await response.json();
+    } catch (error) {
+        throw error;
+    }
+};
+
+export const getPopular = async () => {
+    try {
+        const response = await fetch(
+            `https://api.themoviedb.org/3/person/popular?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
+        );
+        if (!response.ok) {
+            throw new Error(response.json().message);
+        }
+
+        return await response.json();
+    } catch (error) {
+        throw error;
+    }
+};
+
+export const getLanguages = async () => {
+    try {
+        const response = await fetch(
+            `https://api.themoviedb.org/3/configuration/languages?api_key=${process.env.REACT_APP_TMDB_KEY}`
+        );
+        if (!response.ok) {
+            throw new Error(response.json().message);
+        }
+
+        return await response.json();
+    } catch (error) {
+        throw error;
+    }
+};
