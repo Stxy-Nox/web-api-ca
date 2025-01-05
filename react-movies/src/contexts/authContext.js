1b27b31547560d182ca9e431da7bd0bdf6e678d2
SytxNox <seven_night_w@hotmail.com>
2025-01-05 20:06:57 +0000

    intro login and signup from lab

--- react-movies/src/contexts/authContext.js
+++ react-movies/src/contexts/authContext.js
@@ -1,5 +1,5 @@
 import React, { useState, createContext } from "react";
-import { login, signup } from "../api/movies-api";
+import { login, signup } from "../api/movies-api.js";
 
 export const AuthContext = createContext(null);
 
